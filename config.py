3da1ff8c1bd7f215e04c33b9feddd793a2511eed
Babatunde Sanusi <swisskid95@gmail.com>
2019-10-26 00:45:31 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+database_url = os.getenv('DATABASE_URL')
+
+# TODO IMPLEMENT DATABASE URL
+SSQLALCHEMY_DATABASE_URI = database_url
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
